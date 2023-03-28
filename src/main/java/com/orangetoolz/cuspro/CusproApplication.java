3603f170ea414ef065f76e19887ca93eeb641ab1
newaz08 <newazsharifamit@yahoo.com>
2023-03-29 05:25:16 +0600

    initial commit

---
+++ src/main/java/com/orangetoolz/cuspro/CusproApplication.java
@@ -0,0 +1,13 @@
+package com.orangetoolz.cuspro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CusproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CusproApplication.class, args);
+	}
+
+}
